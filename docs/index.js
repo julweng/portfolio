5d680da8e7c7a6c4c38f9dbb22b163abbbe4c325
julweng <julweng@gmail.com>
2018-02-28 08:20:34 -0800

    first draft

--- docs/index.js
+++ docs/index.js
@@ -1,12 +1,16 @@
 'use strict';
 
-const nav = $('nav');
+const navFooter = $('nav, footer');
 const header = $('header');
+const all = $('nav, footer, header, section');
 const headerAbout = $("header a[href='#about']");
 const headerWork = $("header a[href='#work']");
 const headerContact = $("header a[href='#contact']");
 const headerLinks = $("header a[href='#about'], header a[href='#work'], header a[href='#contact']");
-const banner = $("header a[href='#banner']");
+const navBanner = $("nav a[href='#banner']");
+const navAbout = $("nav a[href='#about']");
+const navWork = $("nav a[href='#work']");
+const navContact = $("nav a[href='#contact']");
 const portrait = $('#portrait');
 const about = $('#about');
 const work = $('#work');
@@ -26,11 +30,29 @@ function handleMouseOut() {
   })
 }
 
-// handle header links click
+// handle header or nav link clicks
+function handleNavigation(link, sectionToShow) {
+  link.click((e) => {
+    e.preventDefault();
+    e.stopPropagation();
+    all.addClass('hidden');
+    sectionToShow.removeClass('hidden');
+    if(header.hasClass('hidden')) {
+      navFooter.removeClass('hidden');
+    }
+  })
+}
 
 $(function() {
   handleMouseOver(headerAbout, 'left-portrait');
   handleMouseOver(headerWork, 'center-portrait');
   handleMouseOver(headerContact, 'right-portrait');
   handleMouseOut();
+  handleNavigation(headerAbout, about);
+  handleNavigation(headerWork, work);
+  handleNavigation(headerContact, contact);
+  handleNavigation(navAbout, about);
+  handleNavigation(navWork, work);
+  handleNavigation(navContact, contact);
+  handleNavigation(navBanner, header);
 })
